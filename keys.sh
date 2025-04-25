0dca48084ecbe3873b9570e575fcf902eee3938a
asmairfan04 <65673512+asmairfan04@users.noreply.github.com>
2025-04-25 10:57:36 -0500

    Add files via upload

---
+++ keys.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# the key used for debugging
+test_key="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"
+
+# one or multiple keys used for running experiments
+declare -a keys=(
+    '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'
+)
